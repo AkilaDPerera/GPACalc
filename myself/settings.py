29f3a26aa003f08bf314e621a23689f72f9bfe2c
Akila D. Perera <akilamatrix@gmail.com>
2016-08-19 20:54:17 +0530

    done

--- myself/settings.py
+++ myself/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'myself.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': "mydatabase"#os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
