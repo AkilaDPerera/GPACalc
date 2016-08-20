8bd238a383288a2cffd7c94e19d8e15f48fcf4f1
Akila D. Perera <akilamatrix@gmail.com>
2016-08-20 10:36:41 +0530

    dio

--- myself/settings.py
+++ myself/settings.py
@@ -124,11 +124,7 @@ USE_TZ = True
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['ENGINE']='django.db.backends.postgresql'
-DATABASES['default']['NAME']='people'
-DATABASES['default']['USER']='postgres'
-DATABASES['default']['PASSWORD']='maadpAa@452263293'
-
+DATABASES['default'] =  dj_database_url.config()
 
 
 
