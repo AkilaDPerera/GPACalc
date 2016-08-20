b895a08fd7a1e930f0577c0ec0707fca21c9976f
Akila D. Perera <akilamatrix@gmail.com>
2016-08-20 09:13:23 +0530

    done

--- myself/settings.py
+++ myself/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'people',
         'USER': 'postgres',
         'PASSWORD': 'maadpAa@452263293',
-        'HOST': 'localhost',
+        'HOST': 'https://akiladperera.herokuapp.com/',
         'PORT': '',
     }
 }
