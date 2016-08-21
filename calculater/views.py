09b0750bf521c2c27a6633107a8be1b35a81b20e
Akila D. Perera <akilamatrix@gmail.com>
2016-08-21 09:05:20 +0530

    single pickle

--- calculater/views.py
+++ calculater/views.py
@@ -24,21 +24,12 @@ def signin(request):
         password = request.POST['password']
         
         realName, indexNumber, semesters = LOGIC.SCRAPE(username, password)
-        
-        #OBJ serializing
-        pickle_out = open('realName.pickle', 'wb')
-        pickle.dump(realName, pickle_out)
+
+        #Serializing
+        pickle_out = open('data.pickle', 'wb')
+        pickle.dump({'realName':realName, 'indexNumber':indexNumber, 'semesters':semesters}, pickle_out)
         pickle_out.close()
-        
-        pickle_out = open('indexNumber.pickle', 'wb')
-        pickle.dump(indexNumber, pickle_out)
-        pickle_out.close()
-        
-        pickle_out = open('semesters.pickle', 'wb')
-        pickle.dump(semesters, pickle_out)
-        pickle_out.close()
-        
-        
+
         #Handling error situations
         if realName==-2:
             return render(request, 'calc/signin_exception.html')
@@ -59,32 +50,31 @@ def signin(request):
 def choice1(request):
     
     if request.method=='POST':
-        semChoice = 'BSc Eng. Semester - '+ str(request.POST["semester"])
-        
-        #OBJ Serialization
-        pickle_out = open('semChoice.pickle', 'wb')
-        pickle.dump(semChoice, pickle_out)
-        pickle_out.close()
-        
+
         #OBJ de-serailization
-        pickle_in = open('realName.pickle', 'rb')
-        realName = pickle.load(pickle_in)
+        pickle_in = open('data.pickle', 'rb')
+        data = pickle.load(pickle_in)
         pickle_in.close()
-        
-        pickle_in = open('indexNumber.pickle', 'rb')
-        indexNumber = pickle.load(pickle_in)
+
+        semChoice = 'BSc Eng. Semester - '+ str(request.POST["semester"])
+        data['semChoice'] = semChoice
+
+        #OBJ Serialization
+        pickle_out = open('data.pickle', 'wb')
+        pickle.dump(data, pickle_out)
+        pickle_out.close()
+
+        #OBJ de-serailization
+        pickle_in = open('data.pickle', 'rb')
+        data = pickle.load(pickle_in)
         pickle_in.close()
-        
-        pickle_in = open('semesters.pickle', 'rb')
-        semesters = pickle.load(pickle_in)
-        pickle_in.close()
-        
-    
-        moduleList = semesters[semChoice]
+
+        moduleList = data['semesters'][data['semChoice']]
         moduleList.sort(key=lambda x: x.credit, reverse=True)
-    
-        return render(request, 'calc/successSecond.html', {'semester':semChoice, 'name':realName, 'index':indexNumber, 'modules':moduleList})
+
         
+    
+        return render(request, 'calc/successSecond.html', {'semester':data['semChoice'], 'name':data['realName'], 'index':data['indexNumber'], 'modules':moduleList})
     else:
         return redirect('/calc/signin/')
 
@@ -92,39 +82,35 @@ def choice1(request):
 def choice2(request):
     
     if request.method=='POST':
+
+        #OBJ de-serailization
+        pickle_in = open('data.pickle', 'rb')
+        data = pickle.load(pickle_in)
+        pickle_in.close()
         
         req = request.POST
+        data['req'] = request.POST
         
-        #Serialization
-        pickle_out = open('request.pickle', 'wb')
-        pickle.dump(req, pickle_out)
+        #OBJ Serialization
+        pickle_out = open('data.pickle', 'wb')
+        pickle.dump(data, pickle_out)
         pickle_out.close()
         
         #OBJ de-serailization
-        pickle_in = open('realName.pickle', 'rb')
-        realName = pickle.load(pickle_in)
-        pickle_in.close()
-        
-        pickle_in = open('indexNumber.pickle', 'rb')
-        indexNumber = pickle.load(pickle_in)
-        pickle_in.close()
-        
-        pickle_in = open('semesters.pickle', 'rb')
-        semesters = pickle.load(pickle_in)
-        pickle_in.close()
-        
-        pickle_in = open('semChoice.pickle', 'rb')
-        semChoice = pickle.load(pickle_in)
+        pickle_in = open('data.pickle', 'rb')
+        data = pickle.load(pickle_in)
         pickle_in.close()
 
-        moduleList = semesters[semChoice]
+    
+
+        moduleList = data['semesters'][data['semChoice']]
         moduleList.sort(key=lambda x: x.credit, reverse=True)
     
         
-        LOGIC.ADDINGGRADE(moduleList, req)
+        LOGIC.ADDINGGRADE(moduleList, data['req'])
         GPA = LOGIC.CALCGPA(moduleList)
         
-        return render(request, 'calc/successFinal.html', {'semester':semChoice, 'name':realName, 'index':indexNumber, 'modules':moduleList, 'GPA':GPA, 'post':Feedback.objects.order_by('-date')[:10]})
+        return render(request, 'calc/successFinal.html', {'semester':data['semChoice'], 'name':data['realName'], 'index':data['indexNumber'], 'modules':moduleList, 'GPA':GPA, 'post':Feedback.objects.order_by('-date')[:10]})
 
     else:
         return redirect('/calc/signin/')
@@ -138,38 +124,24 @@ def choice2_post(request):
             comment = request.POST['message']
             
             #OBJ de-serailization
-            pickle_in = open('request.pickle', 'rb')
-            req = pickle.load(pickle_in)
+            pickle_in = open('data.pickle', 'rb')
+            data = pickle.load(pickle_in)
             pickle_in.close()
             
-            pickle_in = open('realName.pickle', 'rb')
-            realName1 = pickle.load(pickle_in)
-            pickle_in.close()
+            Feedback.objects.create(name=nameGiven, realName=data['realName'], index=data['indexNumber'], text=comment)
+
             
-            pickle_in = open('indexNumber.pickle', 'rb')
-            indexNumber = pickle.load(pickle_in)
-            pickle_in.close()
             
-            pickle_in = open('semesters.pickle', 'rb')
-            semesters = pickle.load(pickle_in)
-            pickle_in.close()
-            
-            pickle_in = open('semChoice.pickle', 'rb')
-            semChoice = pickle.load(pickle_in)
-            pickle_in.close()
-            
-            Feedback.objects.create(name=nameGiven, realName=realName1, index=indexNumber, text=comment)
-    
-            moduleList = semesters[semChoice]
+            moduleList = data['semesters'][data['semChoice']]
             moduleList.sort(key=lambda x: x.credit, reverse=True)
         
             
-            LOGIC.ADDINGGRADE(moduleList, req)
+            LOGIC.ADDINGGRADE(moduleList, data['req'])
             GPA = LOGIC.CALCGPA(moduleList)
             
             
             
-            return render(request, 'calc/successFinal.html', {'semester':semChoice, 'name':realName1, 'index':indexNumber, 'modules':moduleList, 'GPA':GPA, 'post':Feedback.objects.order_by('-date')[:10]})
+            return render(request, 'calc/successFinal.html', {'semester':data['semChoice'], 'name':data['realName'], 'index':data['indexNumber'], 'modules':moduleList, 'GPA':GPA, 'post':Feedback.objects.order_by('-date')[:10]})
     
     else:
         return redirect('/calc/signin/')
